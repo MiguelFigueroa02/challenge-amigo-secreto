1d884ae9827a42cca5805faed3f9164b27148902
Miguel Figueroa <miguefigueroa02@gmail.com>
2025-01-27 19:21:43 -0600

    Se carga función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById('amigo').value;
+    console.log(nombreDeAmigo);
+
+    if(nombreDeAmigo === null || nombreDeAmigo.trim() ===""){
+        alert ("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nombreDeAmigo);
+        limpiarCaja();
+    }
+}
+
+function limpiarCaja() {
+    let valorCaja = document.getElementById('amigo').value = '';
+}
